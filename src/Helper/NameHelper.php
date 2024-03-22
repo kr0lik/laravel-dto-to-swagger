ab2cc83c445a22d191e21c83790a35c74459a702
krolik <krolik@yandex.ru>
2024-03-23 00:57:10 +0300

    refactor

--- src/Helper/NameHelper.php
+++ src/Helper/NameHelper.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace Kr0lik\DtoToSwagger\Helper;
 
 use Kr0lik\DtoToSwagger\Attribute\Name;
+use Kr0lik\DtoToSwagger\Contract\JsonResponseInterface;
+use Kr0lik\DtoToSwagger\ReflectionPreparer\Helper\ClassHelper;
 use ReflectionAttribute;
 use ReflectionProperty;
 use Spatie\LaravelData\Attributes\MapInputName;
@@ -21,7 +23,7 @@ class NameHelper
                 return $attributeInstance->name;
             }
 
-            $name = self::fromLaravelData($attribute);
+            $name = self::fromLaravelData($attribute, $reflectionProperty);
 
             if (null !== $name && '' !== $name) {
                 return $name;
@@ -31,16 +33,18 @@ class NameHelper
         return $reflectionProperty->getName();
     }
 
-    private static function fromLaravelData(ReflectionAttribute $attribute): ?string
+    private static function fromLaravelData(ReflectionAttribute $attribute, ReflectionProperty $reflectionProperty): ?string
     {
         if (class_exists(MapInputName::class) && class_exists(MapOutputName::class)) {
             $attributeInstance = $attribute->newInstance();
 
-            if ($attributeInstance instanceof MapInputName) {
+            $isResponse = ClassHelper::isImplementsRecursively($reflectionProperty->getDeclaringClass(), JsonResponseInterface::class);
+
+            if (!$isResponse && $attributeInstance instanceof MapInputName) {
                 return (string) $attributeInstance->input;
             }
 
-            if ($attributeInstance instanceof MapOutputName) {
+            if ($isResponse && $attributeInstance instanceof MapOutputName) {
                 return (string) $attributeInstance->output;
             }
         }
