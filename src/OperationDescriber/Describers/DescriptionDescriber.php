c4eb57f102c4bff38a323c8a7879c2e29aa6faa2
krolik <krolik@yandex.ru>
2024-04-14 19:09:10 +0300

    feat: add description operation describer

---
+++ src/OperationDescriber/Describers/DescriptionDescriber.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kr0lik\DtoToSwagger\OperationDescriber\Describers;
+
+use InvalidArgumentException;
+use Kr0lik\DtoToSwagger\OperationDescriber\OperationDescriberInterface;
+use Kr0lik\DtoToSwagger\ReflectionPreparer\PhpDocReader;
+use OpenApi\Annotations\Operation;
+use ReflectionMethod;
+
+class DescriptionDescriber implements OperationDescriberInterface
+{
+    public function __construct(
+        private PhpDocReader $phpDocReader,
+    ) {}
+
+    /**
+     * @param array<string, mixed> $context
+     *
+     * @throws InvalidArgumentException
+     */
+    public function describe(Operation $operation, ReflectionMethod $reflectionMethod, array $context = []): void
+    {
+        $description = $this->phpDocReader->getDescription($reflectionMethod);
+
+        if ('' !== $description) {
+            $operation->description = $description;
+        }
+
+        if ($this->phpDocReader->isDeprecated($reflectionMethod)) {
+            $operation->deprecated = true;
+        }
+    }
+}
